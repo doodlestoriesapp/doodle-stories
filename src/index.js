b14b2c8699b0f0ebe3347da4911ce5cb4831f901
doodlestoriesapp <doodlestoriesapp@gmail.com>
2026-05-12 20:52:49 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { register } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +9,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register service worker for PWA support
+// This enables offline use and "Add to Home Screen" on iOS/Android
+register();
